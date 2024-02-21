3b796590f2bf5038a0dfb16917a5f0367cec4797
saif580 <saifulhasan580@gmail.com>
2024-02-21 17:06:04 +0000

    installed nodemon and dotenv to protect apikey

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import axios from 'axios'; // Import Axios
 import bodyParser from "body-parser";
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const app = express();
 app.use(express.json());
@@ -19,9 +22,11 @@ app.post('/', async (req, res) => {
     const { email, firstName, lastName } = req.body; // Assuming your form has fields named email, firstName, and lastName
 
     // Mailchimp API credentials
-    const apiKey = '1d168395a8db7f5e3df67d789f6044ac-us13';
+    const apiKey = process.env.API_KEY;
     const audienceId = '892ca8ca7e';
+    console.log(process.env)
     const serverPrefix = apiKey.split('-')[1]; // Extract server prefix from API key
+    console.log(serverPrefix)
 
     // Mailchimp API URL
     const url = `https://${serverPrefix}.api.mailchimp.com/3.0/lists/${audienceId}/members`;
@@ -43,13 +48,14 @@ app.post('/', async (req, res) => {
                 'Authorization': `Bearer ${apiKey}`,
             }
         });
-        res.sendFile(process.cwd()+"/success.html");
+        res.sendFile(process.cwd() + "/success.html");
     } catch (error) {
-            res.sendFile(process.cwd()+"/failure.html");
+        console.log(error);
+        res.sendFile(process.cwd() + "/failure.html");
     }
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// const PORT = 3000;
+app.listen(4000, () => {
+    console.log(`Server is running on port 4000`);
 });
